f2f02e7347622130547d689cc3a40d3a6a926a77
PalakJain9 <palakb188@gmail.com>
2023-02-19 19:36:50 +0530

    file restructure

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+import { Icon } from '@iconify/react';
+
+const Navbar = () => {
+  return (
+    <div className='outer-nav'>
+      <Icon icon="iconoir:emoji-blink-right" color="#fca311" width="35" height="35" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/shortener/Shortener.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import './shortener.css'
+import { Icon } from '@iconify/react';
+
+const Shortener = () => {
+  const [longUrl, setLongUrl] = useState("")
+  useEffect(() => {
+    setLongUrl(longUrl);
+    alert(longUrl)
+  },[longUrl])
+
+  const inputUrl = (event) => { 
+    //if enter pressed
+    if(event.keyCode == 13) { 
+      setLongUrl(event.target.value);
+      event.preventDefault();
+    }
+    //else take long url at time of btn click
+  }
+
+  const shrinkIt = () => {
+    const inputarea = document.getElementById("url-type")
+    setLongUrl(inputarea.value)
+  }
+
+  return (
+    <>
+    <div className='container'>
+      <h2 className='heading'>A tiny URL Shortener for your next <span className='pen-effect'>big project</span></h2>
+      <div className='input-area'>
+        <input id='url-type' name='longUrl' type='text' placeholder='Enter URL here' onKeyDown={(event) => inputUrl(event)}></input>
+        <button classname='btn-black' id='btn_shorten' onClick={() => shrinkIt()}>shorten</button>
+        <button classname='btn-black' id='btn-loader'><Icon icon="eos-icons:bubble-loading" color="white" width="1.5rem" height="1.5rem" inline={true} /></button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Shortener
