42c9d866c8e945db34cbf17541411098bb06cfe0
PalakJain9 <palakb188@gmail.com>
2023-03-02 16:55:07 +0530

    fetch results animation

--- client/src/Routing.jsx
+++ client/src/Routing.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { ReactDOM } from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import App from './App.jsx'
+import Retrieve from './components/retrieve/Retrieve.jsx';
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} exact/>
+        <Route path='/:shortCode' element={<Retrieve />} />
         <Route path='*' element={<div>page not found</div>} />
       </Routes>
     </BrowserRouter>
