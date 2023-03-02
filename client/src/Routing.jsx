20b10b4f2a9537db3ad853ead8d0c8f87294c317
PalakJain9 <palakb188@gmail.com>
2023-03-02 16:12:20 +0530

    routing

---
+++ client/src/Routing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ReactDOM } from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import App from './App.jsx'
+
+const Routing = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} exact/>
+        <Route path='*' element={<div>page not found</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Routing
